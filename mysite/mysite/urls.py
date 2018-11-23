f0cde84d8c721cfd930d735aa922473958e6ceb5
sergey <inoks@mail.ru>
2018-11-23 21:49:59 +0200

    Added SearchIndex configuration and search page.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('polls.urls')),
+    path('search/', include('haystack.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ mysite/polls/search_indexes.py
@@ -0,0 +1,16 @@
+from django.utils import timezone
+from haystack import indexes
+
+from .models import Question
+
+
+class QuestionIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return Question
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(pub_date__lte=timezone.now())
